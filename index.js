7b056e144a8f6c287bdcb4879a242f0349e85a25
shubh agrwal <shubhagarwal123123@gmail.com>
2023-02-17 12:41:13 +0530

    added two way communication

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const express=require('express')
 const http=require('http');
 const app=express();
 const server=http.createServer(app);
+const socketio=require('socket.io');
 const io=socketio(server);
-
+//note to send the event we send socket.emit  and to receive the event we write socket.on
 io.on('connection',(socket)=>{
     console.log('a user connected',socket.id);
 
-    socket.on('from client',()=>{
+    socket.on('from_client',()=>{
         console.log('event getting from client')
     })
+    setInterval(() => {
+        socket.emit('from_server')
+    }, 2000);
 })
-app.use('/',express.static(__dirname+'public'));
-app.listen(3000,()=>{
+app.use('/',express.static(__dirname+'/public'));
+server.listen(3000,()=>{
     console.log("server starting ")
 })
--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
-var soc
+var socket = io();
+
+let btn=document.getElementById('btn');
+btn.onClick=function exec(){
+    socket.emit('from_client')
+    
+}
+socket.on('from_server',()=>{
+    console.log('connecting a new element from server')
+    const div=document.createElement('div');
+    div.innerText= 'element from server';
+    document.body.appendChild(div);
+})
